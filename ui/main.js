b84e82f5b24157a4627f5cb2acfed62e8d84cc20
MarSara <marsara9@gmail.com>
2022-12-16 15:13:50 -0500

    initial commit

---
+++ ui/main.js
@@ -0,0 +1,10 @@
+$(function() {
+    $("div.template").each(function() {
+        let url = $(this).attr("data-content")
+        if(url) {
+            $(this).load(url)
+        } else {
+            console.log("Warning: template with no content url.")
+        }
+    })
+});
