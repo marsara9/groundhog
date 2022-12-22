2ed2ac47b76b8a90ddd5a3e3b57bebc6cf309f23
MarSara <marsara9@gmail.com>
2022-12-22 12:21:04 -0500

    continuing work on setup page

--- ui/setup/setup.js
+++ ui/setup/setup.js
@@ -11,7 +11,28 @@ function updateInterface() {
     $("#passphrase").val("")
 }
 
+function setupTabs() {
+    $(".tabbar").each(function() {
+        const tabs = $(this).children("button.tab")
+        tabs.each(function() {
+            $($(this).attr("data-content")).hide()
+            $(this).click(function() {
+                tabs.each(function() {
+                    $(this).removeClass("selected")
+                    $($(this).attr("data-content")).hide()
+                })
+
+                $(this).addClass("selected")
+                $($(this).attr("data-content")).show()
+            })
+        })
+        tabs.first().click()
+    })
+}
+
 $(document).ready(function() {
+    setupTabs()
+
     $("#wan").change(function() {
         updateInterface()
     })
