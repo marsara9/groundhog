116ab1a1a33dc379d5635be999650256111c6d02
Parker Day <marsara9@gmail.com>
2023-01-03 17:04:04 -0500

    cleaning up unneeded code

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR="${SCRIPT_DIR}/.."
 cd "${PROJECT_DIR}" || { echo >&2 "Project directory doesn't exist?"; exit 1; }
