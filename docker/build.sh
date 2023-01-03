b62f741b514b6166c98cc56f3d776ffacad40e86
Parker Day <marsara9@gmail.com>
2023-01-03 17:24:30 +0000

    adjusting build file

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,10 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR="${SCRIPT_DIR}/.."
-cd "${PROJECT_DIR}"
+cd "${PROJECT_DIR}" || { echo >&2 "Project directory doesn't exist?"; exit 1; }
 docker container stop vpnrouter
 docker container prune -f
 docker image prune -f
-docker build -f docker/dockerfile -t marsara9/vpnrouter:latest .
+docker build -f docker/dockerfile -t marsara9/vpnrouter:latest . || exit 1;
 if [ $1 = "install" ]
 then
     docker compose -f docker/docker-compose.yml up -d
