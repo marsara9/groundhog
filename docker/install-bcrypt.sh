3d4f37dcf4bd0805ddd8529cbab5f5ad2a0663ff
Parker Day <marsara9@gmail.com>
2023-01-16 09:04:16 -0500

    Adding more comments and documentation to existing scripts.

--- docker/install-bcrypt.sh
+++ docker/install-bcrypt.sh
@@ -3,6 +3,11 @@ if [ "$1" = "linux/arm/v7" ]
 then
     echo "Installing from Source"
 
+    ## This is a workaround for https://github.com/rust-lang/rustup/issues/2700.
+    ## Alternatively, if cryptopraphy ever uploads an armv7 wheel, I should be
+    ## able to remove the rust dependency entirely, but they have some roadblocks 
+    ## as outlined here: https://github.com/pyca/cryptography/issues/6286.
+
     mkdir -p $HOME/.cargo
     chmod 777 $HOME/.cargo
     mount -t tmpfs none $HOME/.cargo
